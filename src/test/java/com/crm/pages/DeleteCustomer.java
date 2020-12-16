f331636688159af21294c8ea169cf5f559108212
Yukesh Kumar <yukeshdinesh@gmail.com>
2020-12-16 13:59:37 +0530

    Commiting test to branch US-1

---
+++ src/test/java/com/crm/pages/DeleteCustomer.java
@@ -0,0 +1,12 @@
+package com.crm.pages;
+
+import com.crm.qa.base.TestBase;
+
+public class DeleteCustomer extends TestBase{
+	
+	public DeleteCustomer() {
+		super();
+		System.out.println("Delete customer class added");
+	}
+
+}
